4f89749f8eb12a463f5ca69aedb3e95e6417238a
mauricio mitre <mitremauricio@gmail.com>
2022-03-17 18:15:08 -0300

    Segundo commit

--- Fundamentos/HolaMundo/src/Seccion3.java
+++ Fundamentos/HolaMundo/src/Seccion3.java
@@ -1,6 +1,32 @@
 
 public class Seccion3 {
     public static void main(String[] args) {
+               //Tipos primitivos enteros(byte,short, int ,long)
+//        byte numeroByteMinimo = -127;
+//        byte numeroByteMaximo = (byte) 129;
+//        System.out.println("Numero byte minimo: " + Byte.MIN_VALUE);
+//        System.out.println("Numero byte maximo: " + Byte.MAX_VALUE);
         
+//        short numeroShort = (short)32768;
+//        System.out.println("numeroShort = " + numeroShort);
+//        System.out.println("Numero short minimo: " + Short.MIN_VALUE);
+//        System.out.println("Numero short maximo: " + Short.MAX_VALUE);
+        
+//        int numeroInt = (int)2147483649L;
+//        System.out.println("numeroInt = " + numeroInt);
+//        System.out.println("Numero int minimo: " + Integer.MIN_VALUE);
+//        System.out.println("Numero int maximo: " + Integer.MAX_VALUE);
+        
+//        long numeroLong =9223372036854775807L;
+//        System.out.println("numeroLong = " + numeroLong);
+//        System.out.println("Numero int minimo: " + Long.MIN_VALUE);
+//        System.out.println("Numero int maximo: " + Long.MAX_VALUE);
+
+            //Tipos primitivos flotantes (Float , Double)
+            
+//            float numeroFloat =10.0F;
+//            System.out.println("numeroFloat = " + numeroFloat);
+//            System.out.println("Valor minimo float:"+Float.MIN_VALUE);    
+//            System.out.println("Valor minimo float:"+Float.MAX_VALUE);
     }
 }
